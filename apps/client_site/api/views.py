0c247a6003f95c5e1d30842e2459d6296d17647e
kennedy <kennedyeziechina@gmail.com>
2023-06-12 17:48:42 +0100

    Fixed sitemap issue

--- apps/client_site/api/views.py
+++ apps/client_site/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.request import Request
+from twilio.rest import Client
 
 
 @api_view(["POST"])
@@ -13,7 +14,19 @@ def booking(request: Request) -> Response:
 
 @api_view(["POST"])
 def contact(request: Request) -> Response:
-    print(request.data)
+    message = f'{request.data["your-name"]} with phone number {request.data["your-phone"]} wants {request.data["your-subject"]}. Please Call her!'
+
+    account_sid = 'AC24974abf4514d372f564c0278bf99941'
+    auth_token = 'fb1c45505879c51bec5962926276aff3'
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        from_='+12518664540',
+        body=message,
+        to='+2348141524904'
+    )
+
+    print(message.sid)
 
     return Response({
         'success': True,
